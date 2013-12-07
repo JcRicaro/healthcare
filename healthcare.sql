3abde4b8f7b667cb9ee6fc9fdcaf183cb362707f
JC Ricaro <jeycir@gmail.com>
2013-12-06 17:23:33 -0800

    Database commit yow
    ~Jc Pogi

--- healthcare.sql
+++ healthcare.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2013 at 06:21 AM
+-- Generation Time: Dec 07, 2013 at 09:22 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -38,15 +38,15 @@ CREATE TABLE IF NOT EXISTS `consultations` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `consultations`
 --
 
 INSERT INTO `consultations` (`id`, `patient_id`, `user_id`, `examination`, `observation`, `prescription`, `created_at`, `updated_at`) VALUES
-(8, 1, 4, 'qwe', 'qwe', 'qwe', NULL, NULL),
-(9, 1, 0, 'wwewewew', 'wewewe', 'qwe', NULL, NULL);
+(15, 4, 4, 'qwe', 'qwe', NULL, '2013-12-06 22:47:41', NULL),
+(16, NULL, 4, 'sdsds', 'sdsad', NULL, '2013-12-07 00:20:36', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,28 +72,24 @@ CREATE TABLE IF NOT EXISTS `files` (
 
 CREATE TABLE IF NOT EXISTS `patients` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) DEFAULT NULL,
   `firstname` varchar(50) DEFAULT NULL,
   `lastname` varchar(50) DEFAULT NULL,
   `middlename` varchar(50) DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
+  `age` varchar(50) DEFAULT NULL,
   `contact` varchar(50) DEFAULT NULL,
   `nationality` varchar(50) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
   `civilstatus` varchar(5) DEFAULT NULL,
-  `gender` char(5) DEFAULT NULL,
+  `gender` char(6) DEFAULT NULL,
+  `weight` varchar(50) DEFAULT NULL,
+  `height` varchar(50) DEFAULT NULL,
+  `blood_type` varchar(50) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `patients`
---
-
-INSERT INTO `patients` (`id`, `user_id`, `firstname`, `lastname`, `middlename`, `birthdate`, `contact`, `nationality`, `address`, `civilstatus`, `gender`, `created_at`, `updated_at`) VALUES
-(1, 1, 'JC', 'qwe', 'qwe', NULL, NULL, '', 'qwe', '0', '0', NULL, NULL),
-(2, 0, 'qwee', 'qwe', NULL, NULL, NULL, 'qwe', 'qwe', 'on', 'male', NULL, NULL);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -110,22 +106,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `firstname` varchar(50) DEFAULT NULL,
   `middlename` varchar(50) DEFAULT NULL,
   `lastname` varchar(50) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
+  `age` char(50) DEFAULT NULL,
   `gender` varchar(50) DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `specialization` varchar(50) DEFAULT NULL,
+  `consultation` varchar(50) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `usertype_id`, `phone`, `firstname`, `middlename`, `lastname`, `age`, `gender`, `birthdate`, `email`, `specialization`, `created_at`, `updated_at`) VALUES
-(4, 'username', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 1, NULL, 'qw', NULL, 'qwe', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `password`, `usertype_id`, `phone`, `firstname`, `middlename`, `lastname`, `age`, `gender`, `birthdate`, `email`, `specialization`, `consultation`, `created_at`, `updated_at`) VALUES
+(4, 'username', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 1, NULL, 'qw', NULL, 'qwe', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
